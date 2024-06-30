686f9909d2aa754352821d940ca09eb51309ddef
xMarkhus <marcosmiyamotoshigeru@gmail.com>
2024-06-30 12:28:21 -0300

    commit integração

--- Pt1_base_de_dados.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('cancelamentos.csv')
-display(df)
-
-
-df_atualizado = df.drop('CustomerID', axis=1)
-display(df_atualizado)
-
--- Pt2_tratar_erros.py
+++
@@ -1,9 +0,0 @@
-from Pt1_base_de_dados import df_atualizado
-
-# verificando se há valores vazios
-display(df_atualizado.info())
-
-# excluindo linhas com valores vazios
-df_tratado = df_atualizado.dropna()
-display(df_tratado.info())
-
